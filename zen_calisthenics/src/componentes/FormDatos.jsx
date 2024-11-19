7a9354096c29a91c2deb43423cb6efb491cc5819
Genesis1821 <castillocaprazog@uvm.edu.ve>
2024-11-19 15:35:17 -0400

    cambios

--- zen_calisthenics/src/componentes/FormDatos.jsx
+++ zen_calisthenics/src/componentes/FormDatos.jsx
@@ -5,6 +5,14 @@ import './Forms.css'
 function FormDatos(){
     return(
         <form method='post' className='formCrear'>
+            <select>
+                <option value={option1} className='select'>{option1}</option>
+                <option value={option2} className='select'>{option2}</option>
+                <option value={option3} className='select'>{option3}</option>
+                <option value={option4} className='select'>{option4}</option>
+                <option value={option5} className='select'>{option5}</option>
+                <option value={option6} className='select'>{option6}</option>
+            </select>
         </form>
     )
 };
